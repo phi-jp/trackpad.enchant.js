f9c07a878115e3c453492879be62f3e2a161172d
phi <phi1618jp@gmail.com>
2012-04-18 01:32:48 +0900

    引数でサイズを指定できるよう修正 可動範囲を指定できるよう修正

--- src/trackpad.enchant.js
+++ src/trackpad.enchant.js
@@ -13,11 +13,11 @@
         /**
          * 初期化
          */
-        initialize: function() {
+        initialize: function(width, height) {
             enchant.Entity.call(this);
             
-            this.width = TRACKPAD_WIDTH;
-            this.height= TRACKPAD_HEIGHT;
+            this.width = width  || TRACKPAD_WIDTH;
+            this.height= height || TRACKPAD_HEIGHT;
             this.backgroundColor = TRACKPAD_COLOR;
             
             // 位置調整
@@ -29,6 +29,12 @@
             var style = this._element.style;
             style.margin = style.padding = "0px";
             style.borderRadius = "4px";
+            
+            // 範囲設定
+            this.minX = 0;
+            this.maxX = game.width;
+            this.minY = 0;
+            this.maxY = game.height;
         },
         
         /**
@@ -39,14 +45,13 @@
             e.localX = e.x - this._offsetX;
             e.localY = e.y - this._offsetY;
             
-            var game = enchant.Game.instance;
             var rateX = e.localX/this.width;
             var rateY = e.localY/this.height;
             
             e.rateX = (rateX < 0) ? 0.0 : ((rateX > 1.0) ? 1.0 : rateX);
             e.rateY = (rateY < 0) ? 0.0 : ((rateY > 1.0) ? 1.0 : rateY);
-            e.trackX = e.rateX * game.width;
-            e.trackY = e.rateY * game.height;
+            e.trackX = e.rateX * this.rangeX + this.minX;
+            e.trackY = e.rateY * this.rangeY + this.minY;
             
             if (this["on" + e.type] != null) this["on" + e.type](e);
             var listeners = this._listeners[e.type];
@@ -56,8 +61,44 @@
                     listeners[i].call(this, e);
                 }
             }
-        }
+        },
         
+        _updateRangeX: function() {
+            this.rangeX = (this.maxX - this.minX);
+        },
+        
+        _updateRangeY: function() {
+            this.rangeY = (this.maxY - this.minY);
+        },
+        
+    });
+    
+    Object.defineProperties(enchant.Trackpad.prototype, {
+        minX: {
+            get: function()  { return this._minX; },
+            set: function(v) { this._minX = v; this._updateRangeX(); }
+        },
+        maxX: {
+            get: function()  { return this._maxX; },
+            set: function(v) { this._maxX = v; this._updateRangeX(); }
+        },
+        minY: {
+            get: function()  { return this._minY; },
+            set: function(v) { this._minY = v; this._updateRangeY(); }
+        },
+        maxY: {
+            get: function()  { return this._maxY; },
+            set: function(v) { this._maxY = v; this._updateRangeY(); }
+        },
+        
+        rangeX: {
+            get: function()  { return this._rangeX; },
+            set: function(v) { this._rangeX = v; },
+        },
+        rangeY: {
+            get: function()  { return this._rangeY; },
+            set: function(v) { this._rangeY = v; },
+        },
     });
     
 })();
